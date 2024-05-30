fa125f4c9c887fb8ccccb1e1502667fb49f176fa
Eduardo Lefèvre <eduardo@lefevre.com>
2024-05-30 14:03:09 -0300

    Code cleanup

--- infra/base/ecr.tf
+++ infra/base/ecr.tf
@@ -1,3 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  ecr_url = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.prefix}-"
+}
+
 resource "aws_ecr_repository" "quotes" {
   name         = "${var.prefix}-quotes"
   force_delete = true
@@ -13,12 +19,6 @@ resource "aws_ecr_repository" "front_end" {
   force_delete = true
 }
 
-data "aws_caller_identity" "current" {}
-
-locals {
-  ecr_url = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.prefix}-"
-}
-
 resource "aws_ssm_parameter" "ecr" {
   name  = "/${var.prefix}/base/ecr"
   value = local.ecr_url
@@ -28,8 +28,4 @@ resource "aws_ssm_parameter" "ecr" {
 resource "local_file" "ecr" {
   filename = "${path.module}/../ecr-url.txt"
   content  = local.ecr_url
-}
-
-output "repository_base_url" {
-  value = local.ecr_url
 }
